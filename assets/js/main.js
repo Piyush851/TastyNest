f0ae6815243feb38f371f2dd559f5e8ba6a40fd4
Piyush851 <Piyushsaini01@outlook.com>
2025-08-03 14:48:22 +0530

    Added signup and login

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,51 @@ fetch("/TastyNest/components/header.html")
         return response.text();
     })
     .then(html=> {
-        document.getElementById("navbar-placeholder").outerHTML = html;
+        document.getElementById("navbar-placeholder").innerHTML = html;
+        setTimeout(() => {
+            // login modal loading
+            const loginBtn = document.getElementById('loginBtn');
+            if(loginBtn) {
+                loginBtn.addEventListener('click', function () {
+                    if(!document.querySelector('#exampleModal')) {
+                        fetch('/TastyNest/components/login-modal.html')
+                            .then(response => response.text())
+                            .then(data => {
+                                document.getElementById('login-modal-container').innerHTML = data;
+
+                                const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+                                modal.show();
+                                // Signup modal loading
+                                const signupLink = document.getElementById('openSignup');
+                                if(signupLink) {
+                                    signupLink.addEventListener('click', function(e) {
+                                        e.preventDefault();
+                                        const loginModalEl = document.getElementById('exampleModal');
+                                        const loginModal = bootstrap.Modal.getInstance(loginModalEl)
+                                        loginModal.hide();
+                                        // Check is signup modal is already present
+                                        if(!document.querySelector('#signupModal')) {
+                                            fetch('/TastyNest/components/signup-modal.html')
+                                                .then(res => res.text())
+                                                .then(data => {
+                                                    document.getElementById('login-modal-container').insertAdjacentHTML('beforeend', data);
+                                                    const signupModal = new bootstrap.Modal(document.getElementById('signupModal'));
+                                                    signupModal.show();
+                                                });
+                                        } else {
+                                            const signupModal = new bootstrap.Modal(document.getElementById('signupModal'));
+                                            signupModal.show();
+                                        }
+                                    })
+                                }
+                            });
+                    } else {
+                        const modal = new bootstrap.Modal(document.getElementById('exampleModal'));
+                        modal.show();
+                    }
+                });
+            }
+        },0);
     })
     .catch(error => {
         console.error("Error loading navbar: ", error);
@@ -15,6 +59,7 @@ fetch("/TastyNest/components/header.html")
     .finally(()=>{
         document.body.classList.remove("hidden");
     })
+
 // Loading footer
 fetch("/TastyNest/components/footer.html")
     .then(response=>{
@@ -56,3 +101,10 @@ fetch("/TastyNest/components/testimonials.html")
     .catch(error => {
         console.error("Error loading testimonials: ", error);
     })
+
+// // Login modal
+// fetch('/TastyNest/components/header.html')
+//     .then(res => res.text())
+//     .then(data=> {
+//         document.getElementById('navbar-placeholder').innerHTML = data;
+//     });
